115cac5510db5cd479771a801847b94f83be9ff6
Tibor Németh <davacii@proton.me>
2026-04-29 12:49:38 +0200

    showcase

---
+++ scripts/processing.py
@@ -0,0 +1,11 @@
+from manim import *
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # Create a circle
+        circle.set_fill(PINK, opacity=0.5)  # Set color and opacity
+        square = Square()  # Create a square
+
+        self.play(Create(square))  # Animate square creation
+        self.play(Transform(square, circle))  # Transform square into circle
+        self.play(FadeOut(square))  # Fade out
